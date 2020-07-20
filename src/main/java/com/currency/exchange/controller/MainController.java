2523af2f326ae336f0ae91a92de73c1bd6b84c9f
usurinisso <tautvis477@gmail.com>
2020-07-20 20:36:53 +0300

    Nothing serious

--- src/main/java/com/currency/exchange/controller/MainController.java
+++ src/main/java/com/currency/exchange/controller/MainController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 public class MainController {
+
     @GetMapping("/")
     public ModelAndView main() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("index");
         return modelAndView;
     }
+
 }
