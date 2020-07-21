cda0a89a58d0c7c0debf105804717957df8e8542
usurinisso <tautvis477@gmail.com>
2020-07-21 15:27:50 +0300

    Added a query getCurrencyByCcy

--- src/main/java/com/currency/exchange/repository/CurrencyRepository.java
+++ src/main/java/com/currency/exchange/repository/CurrencyRepository.java
@@ -2,8 +2,12 @@ package com.currency.exchange.repository;
 
 import com.currency.exchange.model.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface CurrencyRepository extends JpaRepository<Currency, Integer> {
+    @Query(value = "FROM Currency b where b.ccy = ?1")
+    public Currency getCurrencyByCcy(String ccy);
 }
