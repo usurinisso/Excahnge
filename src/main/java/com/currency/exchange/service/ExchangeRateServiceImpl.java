366df915423c953a1c43906d4b6fcbb2e817baf9
usurinisso <tautvis477@gmail.com>
2020-07-21 15:29:12 +0300

    Now sets the currency for currencyAmount

--- src/main/java/com/currency/exchange/service/ExchangeRateServiceImpl.java
+++ src/main/java/com/currency/exchange/service/ExchangeRateServiceImpl.java
@@ -1,6 +1,5 @@
 package com.currency.exchange.service;
 
-import com.currency.exchange.model.CurrencyAmount;
 import com.currency.exchange.model.ExchangeRate;
 import com.currency.exchange.repository.CurrencyAmountRepository;
 import com.currency.exchange.repository.CurrencyRepository;
@@ -14,13 +13,15 @@ import java.util.Arrays;
 public class ExchangeRateServiceImpl implements ExchangeRateService {
 
     private final ExchangeRateRepository exchangeRateRepository;
+    private final CurrencyRepository currencyRepository;
     private final CurrencyAmountRepository currencyAmountRepository;
 
 
     @Autowired
-    public ExchangeRateServiceImpl (ExchangeRateRepository exchangeRateRepository, CurrencyAmountRepository currencyAmountRepository) {
-        this.currencyAmountRepository = currencyAmountRepository;
+    public ExchangeRateServiceImpl (ExchangeRateRepository exchangeRateRepository, CurrencyRepository currencyRepository, CurrencyAmountRepository currencyAmountRepository) {
+        this.currencyRepository = currencyRepository;
         this.exchangeRateRepository = exchangeRateRepository;
+        this.currencyAmountRepository = currencyAmountRepository;
     }
 
     @Override
@@ -32,5 +33,9 @@ public class ExchangeRateServiceImpl implements ExchangeRateService {
     public void saveAllExchangeRates(ExchangeRate[] ers) {
         Arrays.asList(ers).forEach(item->item.setParent());
         exchangeRateRepository.saveAll(Arrays.asList(ers));
+        currencyAmountRepository.findAll().forEach(item->{
+            item.setCurrency(currencyRepository.getCurrencyByCcy(item.getCcy()));
+            currencyAmountRepository.save(item);
+        });
     }
 }
