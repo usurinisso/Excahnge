19bfa57d8ebf7fb65f21f0e6c6bc39394c6e4fb3
usurinisso <tautvis477@gmail.com>
2020-07-22 22:07:10 +0300

    New query for getting exchange rates

--- src/main/java/com/currency/exchange/repository/ExchangeRateRepository.java
+++ src/main/java/com/currency/exchange/repository/ExchangeRateRepository.java
@@ -2,9 +2,14 @@ package com.currency.exchange.repository;
 
 import com.currency.exchange.model.ExchangeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Integer> {
+    @Query("FROM ExchangeRate b where b.ccyT = ?1 ORDER BY b.dt DESC")
+    public List<ExchangeRate> getExchangeRateByCcy(String ccy);
 }
